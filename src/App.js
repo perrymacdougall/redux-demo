f5bdf7819d6dfa22f30353c5e5c78f61ae6dc70c
perrymacdougall <perrymacdougall@gmail.com>
2019-09-15 21:53:06 -0400

    created a form component that will submit new posts

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import Posts from './components/Posts';
+import PostForm from './components/PostForm';
+
 
 function App() {
   return (
     <div className="App">
+      <PostForm />
+      <hr />
       <Posts />
     </div>
   );
